40b8a850d5c2da52be00809b4c271ec96d2bb1f3
Erin Cry <erin.cry@email.com>
2020-06-25 20:19:05 -0700

    add fadein and fade out(not working yet) to iguana ids

--- js/scripts.js
+++ js/scripts.js
@@ -4,3 +4,17 @@ $(document).ready(function() {
     $("#walrus-hidden").toggle();
   });
 });
+
+$(document).ready(function() {
+  $(".clickable1").click(function() {
+    $("#cat-showing").toggle();
+    $("#cat-hidden").toggle();
+  });
+});
+
+$(document).ready(function() {
+  $(".clickable2").click(function() {
+    $("#iguana-showing").fadeIn();.toggle();
+    $("#iguana-hidden").fadeOut();
+  });
+});
